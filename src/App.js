796277c55a52e38f55a1e32205956a95e48ee80e
Klecianny Melo <kleciannymelo@gmail.com>
2023-07-04 22:32:23 -0300

    :sparkles: implementando a rota /

--- src/App.js
+++ src/App.js
@@ -1,89 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { z } from 'zod';
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
 
-// Esquema de validação utilizando Zod
-const dataSchema = z.array(
-  z.object({
-    id: z.string().nonempty(),
-    url: z.string().url().nonempty(),
-    width: z.number(),
-    height: z.number(),
-  })
-);
-
-const DataValidationPage = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-      // Tempo em milissegundos para exibir a frase "Carregando..."
-    }, 3000);
-
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://api.thecatapi.com/v1/images/search', { method: "GET" });
-        const result = await response.json();
-
-        // Verificando se os dados são válidos
-        const validatedData = dataSchema.parse(result);
-        console.log('Validação dos dados: ', validatedData);
-
-        setData(validatedData);
-      } catch (error) {
-        console.log('Erro de validação: ', error.message);
-        setError('⚠️ A aplicação está passando por instabilidade momentânea. Tente novamente mais tarde.');
-      }
-    };
-    
-    fetchData();
-  }, []);
-
-  const handleReload = () => {
-    window.location.reload();
-  };
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  // !data
-  if (isLoading) {
-    return <div>Carregando...</div>;
-  }
-
-  // Renderizando a aplicação corretamente com os dados válidos
+function App() {
   return (
-    <div>
-      <h1>The cat API 🐈‍⬛</h1>
-      {
-        data.map((cat) => (
-          <div key={ cat.id }>
-            <p><b>Id:</b> { cat.id }</p>
-            <img
-              src={ cat.url }
-              alt={ cat.id }
-              style={{ width: "500px", height: "500px" }}
-            />
-            <p><b>Url:</b> { cat.url }</p>
-            <p><b>Width:</b> { cat.width }</p>
-            <p><b>Heigth:</b> { cat.height }</p>
-            <button
-              onClick={ handleReload }
-              style={{ padding: "10px", width: "500px", backgroundColor: "#696969", border: "solid 1.5px transparent", borderRadius: "5px", fontSize: "35px", fontWeight: "bold"  }}
-              onMouseOver={(e) => e.target.style.backgroundColor = "	#404040"}
-              onMouseOut={(e) => e.target.style.backgroundColor = "#696969"}
-            >
-              NOVO GATO 🐈
-            </button>
-          </div>
-        ))
-      }
+    <div className="App">
+      <BrowserRouter>
+        < Header />
+        <Routes>
+          <Route exact path="/" element={ <Home /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-};
+}
 
-export default DataValidationPage;
-
+export default App;
