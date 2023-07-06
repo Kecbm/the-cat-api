4cd80535dc7491a446af9fcf4a2ebce7a94c9bef
Klecianny Melo <kleciannymelo@gmail.com>
2023-07-05 22:08:46 -0300

    :sparkles: novas rotas: /request e /cats

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
+import Request from './pages/Request';
+import Cats from './pages/Cats';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         < Header />
         <Routes>
           <Route exact path="/" element={ <Home /> } />
+          <Route exact path="/request" element={ <Request /> } />
+          <Route exact path="/cats" element={ <Cats /> } />
         </Routes>
       </BrowserRouter>
     </div>
