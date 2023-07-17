b6f0105d6e319dcbd75c46bd30ec96551a638b17
Klecianny Melo <kleciannymelo@gmail.com>
2023-07-17 18:02:09 -0300

    :sparkles: nova rota: /catforbreed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Header from './components/Header';
 import Request from './pages/Request';
 import Cats from './pages/Cats';
+import CatForBreed from './pages/CatForBreed';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" element={ <Home /> } />
           <Route exact path="/request" element={ <Request /> } />
           <Route exact path="/cats" element={ <Cats /> } />
+          <Route exact path="/catforbreed" element={ <CatForBreed /> } />
         </Routes>
       </BrowserRouter>
     </div>
